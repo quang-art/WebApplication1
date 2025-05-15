c56a573dd13792b41c83e4494bd1ec49d95e272c
Quang <vq787898@gmail.com>
2025-05-15 16:07:01 +0700

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class StudentController : Controller
+	{
+		
+		private static List<Student> Students = new List<Student>();
+
+		
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		
+		[HttpPost]
+		public IActionResult Index(Student student)
+		{
+			if (ModelState.IsValid)
+			{
+				Students.Add(student);
+				return RedirectToAction("ShowKQ", student); 
+			}
+			return View(student);
+		}
+
+		
+		public IActionResult ShowKQ(Student student)
+		{
+			
+			int count = Students.Count(s => s.ChuyenNganh == student.ChuyenNganh);
+			ViewBag.Count = count; 
+			ViewBag.AllStudents = Students; 
+			return View(student); 
+		}
+	}
+}
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Vui lòng nhập MSSV")]
+        public string MSSV { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Họ tên")]
+        public string HoTen { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Điểm TB")]
+        [Range(0, 10, ErrorMessage = "Điểm TB phải từ 0 đến 10")]
+        public float DiemTB { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn Chuyên ngành")]
+        public string ChuyenNganh { get; set; }
+    }
+}
